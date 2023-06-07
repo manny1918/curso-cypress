09bc5da50f0b66c6acfd8135e3b3aa6cf7e5e9c7
Yomara Mora <yomara.mora@clickbank.com>
2023-06-07 19:39:21 +0100

    configuracion de reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
