aed19be70bf8e6e4942f1da9cbac6a58979de310
Yomara Mora <yomara.mora@clickbank.com>
2023-06-08 19:13:44 +0100

    inhabilitar video post ejecucion

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   retries: 1,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
