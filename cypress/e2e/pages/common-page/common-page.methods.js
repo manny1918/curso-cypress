a526174704ba90adb12a3353759f77901ec88156
Yomara Mora <yomara.mora@clickbank.com>
2023-06-09 12:39:02 +0100

    Create usuario aleatorio

--- cypress/e2e/pages/common-page/common-page.methods.js
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -36,4 +36,16 @@ export class CommonPageMethods {
       expect(str).to.equal(expectedMessage);
     });
   }
+
+  static generateRandomString(length = 10) {
+    let result = "";
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
 }
--- cypress/e2e/tests/registro-y-autenticacion.cy.js
+++ cypress/e2e/tests/registro-y-autenticacion.cy.js
@@ -3,6 +3,9 @@ import { CommonPageMethods } from '../pages/common-page/common-page.methods';
 import { SignupMethods } from '../pages/signup/signup.methods';
 import { Logger } from '../util/logger';
 
+const user = CommonPageMethods.generateRandomString();
+const password = CommonPageMethods.generateRandomString(7)
+
 describe(CommomPageData.testSuites.registroYAutenticacion, () => {
   it("Registro de usuario válido", () => {
     Logger.stepNumber(1)
@@ -15,8 +18,8 @@ describe(CommomPageData.testSuites.registroYAutenticacion, () => {
 
     Logger.stepNumber(3)
     Logger.step('Completar todos los campos obligatorios con información válida')
-    SignupMethods.insertUsername('dhfgjkdhgjfgj')
-    SignupMethods.insertPassword('dhfgjkdhgjfgj')
+    SignupMethods.insertUsername(user)
+    SignupMethods.insertPassword(password)
 
     Logger.stepNumber(4)
     Logger.step('Hacer clic en "Sign up" para registrar el usuario')
