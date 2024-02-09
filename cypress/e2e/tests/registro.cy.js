87fa85c8781ef5f51ae63a913d067166b8cd7994
Pedro Lopez <pedrolopez@amazon.com>
2024-02-09 14:02:07 +0000

    Agregar nuevo CP de login

--- cypress/e2e/tests/registro.cy.js
+++ cypress/e2e/tests/registro.cy.js
@@ -50,4 +50,25 @@ describe(CommomPageData.testSuites.registro, () => {
     Logger.verification('Verificar que se muestra un mensaje de error indicando los campos inválidos')
     SignupMethods.verifyThatThisUserAlreadyExistMessageIsDisplayed();
   });
+
+  it("Registro de usuario inválido", () => {
+    Logger.stepNumber(1)
+    Logger.step('Navegar a la página de inicio')
+    CommonPageMethods.navigateToDemoBlaze();
+
+    Logger.stepNumber(2)
+    Logger.step('Hacer clic en "Sign up" en la barra de navegación')
+    CommonPageMethods.clickOnSignupOption();
+
+    Logger.stepNumber(3)
+    Logger.step('Completar algunos campos con información inválida')
+    SignupMethods.insertUsername(existingUser)
+    SignupMethods.insertPassword(password)
+
+    Logger.stepNumber(4)
+    Logger.step('Hacer clic en "Sign up" para registrar el usuario')
+    SignupMethods.clickOnSignupButton();
+    Logger.verification('Verificar que se muestra un mensaje de error indicando los campos inválidos')
+    SignupMethods.verifyThatThisUserAlreadyExistMessageIsDisplayed();
+  });
 });
